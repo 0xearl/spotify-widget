03d6bf79efbbdf6c03c1bde70035829de87f1da0
0xearl <earljohnsabalo@gmail.com>
2022-10-04 15:02:17 +0800

    initial commit

---
+++ backend/routes/index.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var axios = require('axios')
+var querystring = require('querystring');
+var router = express.Router();
+var client_id = 'c265e177c1dc4c0ea41d61d0100a74c3'; // Your client id
+var client_secret = '812aff82b88e49cfb102836db57ca289'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var stateKey = 'spotify_auth_state';
+
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+
+router.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-modify-playback-state user-read-playback-state user-read-currently-playing';
+  res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}&state=${state}`);
+});
+
+router.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  // var authOptions = {
+  //   url: 'https://accounts.spotify.com/api/token',
+  //   headers: { 'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) },
+  //   form: {
+  //     grant_type: 'refresh_token',
+  //     refresh_token: refresh_token
+  //   },
+  //   json: true
+  // };
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+    }
+  }).then(data => {
+    var access_token = data.access_token
+
+    return res.send({
+      'access_token': access_token
+    })
+  })
+
+  // axios.post(authOptions, function(error, response, body) {
+  //   if (!error && response.statusCode === 200) {
+  //     var access_token = body.access_token;
+  //     res.send({
+  //       'access_token': access_token
+  //     });
+  //   }
+  // });
+});
+
+router.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    return res.redirect('/?error=state_mismatch');
+  } else {
+    res.clearCookie(stateKey);
+    let form = {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code'
+    }
+
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify(form),
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(response => {
+      console.log('data', response)
+      var access_token = response.data.access_token,
+          refresh_token = response.data.refresh_token;
+
+      axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        }
+      }).then(data => {
+        console.log(data)
+      })
+
+      return res.redirect(`http://localhost:3000/?access_token=${access_token}&refresh_token=${refresh_token}`)
+    }).catch(err => {
+      console.log(err)
+      return res.redirect('http://localhost:3000/login?error=invalid_token')
+    })
+  }
+});
+
+router.get('/playing', (req, res) => {
+  access_token = req.query.access_token
+  var options = {
+    url: 'https://api.spotify.com/v1/me/player/currently-playing',
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    json: true
+  };
+  axios({
+    method: 'get',
+    url: 'https://api.spotify.com/v1/me/player/currently-playing',
+    headers: {
+      'Authorization': 'Bearer ' + access_token,
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    console.log('response', response.status)
+
+    if(response.status == 204) {
+      return res.json({
+        message: 'Not playing'
+      })
+    } 
+
+    if(response.error) {
+      return res.json({error: response.data.error.message})
+    }
+
+    return res.send(response.data)
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
+module.exports = router;
---
+++ spotifyplayer/src/App.js
@@ -0,0 +1,176 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+const qs = require('query-string');
+
+
+function Login() {
+  return(
+    <div className="w-full h-full">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl justify-center items-center">
+          <a href="http://localhost:8888/login"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Login with spotify
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+function Player(props) {
+  let [artists, setArtists] = useState('')
+  let [songName, setSongName] = useState('')
+  let [songImage, setSongImage] = useState('')
+  let [imageWidth, setImageWidth] = useState('')
+  let [imageHeight, setImageHeight] = useState('')
+  let [time, setTime] = useState(0)
+
+  let access_token = props.AccessToken
+
+  const getPlaying = (callback) => {
+    fetch(`http://localhost:8888/playing?access_token=${access_token}`)
+      .then(res => {
+          return res.json().then(data => {
+            callback(data);
+          })
+      }).catch(err => {
+          console.log('error', err.toString())
+      })
+  }
+  setTimeout(() => {
+    getPlaying((data) => {
+      setArtists(data.item.artists.map((artist) => {
+        return artist.name
+      }).join(', '))
+      setSongName(data?.item.name)
+      setSongImage(data?.item.album.images[0].url)
+      setImageHeight(data.item.album.images[0].height)
+      setImageWidth(data.item.album.images[0].width)
+      setTime((data?.item.duration_ms - data?.progress_ms))
+    })
+  }, time)
+  
+  console.log('songName', songName)
+  console.log('artists', artists)
+  console.log('songImage', songImage)
+  console.log('time remaining', time)
+  return (
+      <div className='relative flex h-full w-full flex-col justify-center overflow-hidden bg-gray-900'>
+        <div className="divide-y overflow-hidden rounded-lg">
+          <div className="h-full">
+            <img src={songImage} width={imageWidth} height={imageHeight}/>
+          </div>
+          <div className="px-4 py-4 sm:px-6">
+            {/* <div className="flex justify-center align-middle items-center mb-4">
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                <div className="bg-indigo-600 h-2.5 rounded-full" style={{width: '45%'}}></div>
+              </div>
+            </div> */}
+            <div className="grid grid-rows-3 grid-flow-col gap-4 justify-center align-middle text-center h-24">
+              <div className="row text-2xl font-mono font-medium text-white">{songName}</div>
+              <div className="row text-sm font-mono text-gray-500">{artists}</div>
+              <div className="row-span-3 col-span-1 ml-auto text-red-500">
+                <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                </svg>
+                </button>
+              </div>
+            </div>
+            <div className="flex justify-between align-middle text-white p-4 py-1 m-5">
+              <div>
+                <button alt="previous">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                  </svg>
+                </button>
+              </div>
+            <div>
+              <button alt="play" id="togglePlay">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                </svg>
+              </button>
+            </div>
+            <div>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+function App() {
+  let queries = qs.parse(window.location.search);
+  let [access_token, setAccessToken] = useState('')
+  let [refresh_token, setRefreshToken] = useState('')
+  let [logged_in, setLoggedIn] = useState(false)
+
+  const RenewToken = () => {
+    useEffect(() => {
+      let new_access_token = refreshToken()
+      setRefreshToken(new_access_token)
+    }, [])
+  }
+  const refreshToken = () => {
+    fetch(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
+      .then(res => {
+        return res.json()
+      }).then(data => {
+        return data.access_token
+      })
+  }
+  setTimeout(() => {
+    RenewToken()
+  }, 60 * 60 * 1000)
+
+  useEffect(() => {
+    if( queries.access_token == '' && queries.refresh_token == ''||
+        queries.access_token == undefined && queries.refreshToken == undefined
+    ) {
+      setLoggedIn(false)
+    } else {
+      setAccessToken(queries.access_token)
+      setRefreshToken(queries.refresh_token)
+      setLoggedIn(true)
+    }
+  }, [])
+  
+  if(!logged_in) {
+    return <Login/>;
+  }
+  return <Player AccessToken={access_token} setAccessToken={setAccessToken} RefreshToken={refresh_token} setRefreshToken={setRefreshToken}/>;
+}
+
+// class App extends React.Component 
+// {
+//   constructor(props) {
+//     super(props)
+//     let url = window.location.search;
+//     this.queries = qs.parse(url)
+//     this.state = {
+//       access_token: '',
+//       refresh_token: '',
+//       logged_in: false,
+//     }
+//   }
+
+
+//   render() {
+//     return (
+//       <div>Hello World</div>
+//     )
+//   }
+// }
+
+
+export default App;
