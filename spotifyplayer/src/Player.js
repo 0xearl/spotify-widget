273691895a020269d1e4a44c8f0feb53310ac0a9
0xearl <earljohnsabalo@gmail.com>
2022-12-29 09:32:41 +0800

    changed layout

--- spotifyplayer/src/Player.js
+++ spotifyplayer/src/Player.js
@@ -115,8 +115,51 @@ class Player extends React.Component {
 
     render() {
         return (
-            <div className="flex h-screen justify-center items-center bg-slate-900">
-                <div className="h-[90%] w-[80%] border-2 border-slate-600 rounded-md bg-slate-800 mt-4">
+            <div className="flex w-[600px] items-center justify-center bg-slate-700">
+                <div className="h-full">
+                    <img width="250" height="250" src={this.state.songImage}/>
+                </div>
+                <div className="w-full ml-4 col-span-2">
+                    <div className="flex flex-col">
+                        <div name="artist-yawa" className="font-mono items-center flex justify-center">
+                            <div className="w-[80%] text-center">
+                                <div className='font-bold text-white justify-center align-middle'>
+                                    {this.state.songName}
+                                </div>
+                                <div className='mt-1 text-slate-500'>{this.state.artists}</div>
+                            </div>
+                        </div>
+                    <div className="flex justify-center items-center m-4">
+                        <div className='w-[350px] border-[0.5px]'></div>
+                    </div>
+                    <div id="controls">
+                        <div className='flex justify-center'>
+                            <div className="flex flex-wrap justify-between items-center w-[15rem] text-white text-2xl mb-3">
+                                <button onClick={() => { this.previous() }} className="hover:text-red-300">↺</button>
+                                <button onClick={() => { this.playPause() }} className="hover:text-red-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                    {
+                                        !this.state.paused ? 
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                                            </svg>
+                                        : 
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                                        </svg>
+                                      
+                                    }
+                                    </svg>
+                                </button>
+                                <button onClick={() => { this.next() }} className="hover:text-red-300">↻</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            /*<div className="flex h-screen justify-center items-center bg-slate-800">
+                <div className="h-full w-full mt-4">
                     <div className="flex justify-center items-center m-16">
                         <img src={this.state.songImage} width="250" className="rounded-lg" alt={this.state.songName}/>
                     </div>
@@ -124,17 +167,16 @@ class Player extends React.Component {
                         <div className="w-[80%] text-center">
                             <div className='font-bold text-white justify-center align-middle'>
                                 {this.state.songName}
-                                {/* <div className='inline-flex align-middle ml-2 text-red-400 hover:text-red-600'>
+                                { <div className='inline-flex align-middle ml-2 text-red-400 hover:text-red-600'>
                                     <button>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hover:fill-red-600">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                         </svg>
                                     </button>
-                                </div> */}
+                                </div> }
                             </div>
                             <div className='mt-1 text-slate-500'>{this.state.artists}</div>
                         </div>
-
                     </div>
                     <div className="flex justify-center items-center m-4">
                         <div className='w-[350px] border-[0.5px]'></div>
@@ -155,7 +197,7 @@ class Player extends React.Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>*/
         );
     }
 }
